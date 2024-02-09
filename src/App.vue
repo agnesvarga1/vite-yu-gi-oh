e9ce5f4158d616d80d337e42e4b09d6dcba0c39b
agnesvarga1 <agikolyok@gmail.com>
2024-02-09 16:40:26 +0100

    main comp structure, api call

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <script>
+import { store } from "./store";
 import AppHeader from "./components/AppHeader.vue";
+import SearchComp from "./components/main_comps/SearchComp.vue";
+import AppMain from "./components/main_comps/AppMain.vue";
+
+import axios from "axios";
 export default {
   name: "App",
+  data() {
+    return {
+      store,
+    };
+  },
   components: {
     AppHeader,
+    SearchComp,
+    AppMain,
+  },
+  methods: {
+    fillArrFromApi: function () {
+      axios.get(store.apiUrl).then((res) => {
+        store.yuCards = res.data;
+      });
+    },
+  },
+  mounted() {
+    this.fillArrFromApi();
   },
 };
 </script>
 
 <template>
   <AppHeader />
+  <main>
+    <SearchComp />
+    <AppMain />
+  </main>
 </template>
 
 <style lang="scss">
